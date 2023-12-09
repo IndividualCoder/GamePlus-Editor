75b12f8d0dc14318e8ed317758277d9dbe8a5cb2
IndividualCoder <121798131+IndividualCoder@users.noreply.github.com>
2023-12-09 20:06:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='GamePlusEditor',
     description='A gui game maker written in python',
-    long_description = "Uses ursina engine as a base"
+    long_description = 'Uses ursina engine as a base',
+    long_description_content_type = `text/x-rst`,
+
     version='0.0.1',
     url='https://github.com/IndividualCoder/GamePlus-Editor',
     author='Prince',
