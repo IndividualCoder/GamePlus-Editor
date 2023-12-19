1b66730fcd11c8b5836b2e6882266ae4cec28765
IndividualCoder <121798131+IndividualCoder@users.noreply.github.com>
2023-12-19 21:44:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     long_description=long_desc,
     long_description_content_type="text/markdown",
 
-    version='0.0.7',
+    version='0.0.8',
     url='https://github.com/IndividualCoder/GamePlus-Editor',
     author='Prince',
     author_email='IndividualCoder@gmail.com',
